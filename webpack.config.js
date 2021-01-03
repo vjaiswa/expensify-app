edd8397e79232c193a4a41973e891f7ef16029d5
Vijay Jaiswal <jais_myvijay@yahoo.co.in>
2021-01-03 15:52:38 -0500

    Final Version

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ if(process.env.NODE_ENV === "development"){
 module.exports = (env) =>{
   const isProduction = env == "production"
   return {
-    entry: './src/app.js',
+    entry: ['babel-polyfill','./src/app.js'],
     output: {
       path: path.join(__dirname, 'public','dist'),
       filename: 'bundle.js'
